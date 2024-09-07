5af71752265a2118854b6f91c598ce5a6218eaa4
lihzsky <lihaizh_cn@foxmail.com>
2024-09-07 15:49:41 +0800

    fix: 优化样式

--- mp-platform/alipay/pages/index/index.js
+++ mp-platform/alipay/pages/index/index.js
@@ -1,43 +1,59 @@
 Page({
-  onLoad(query) {
-    // 页面加载
-    console.info(`Page onLoad with query: ${JSON.stringify(query)}`);
-  },
-  onReady() {
-    // 页面加载完成
-  },
-  onShow() {
-    // 页面显示
-  },
-  onHide() {
-    // 页面隐藏
-  },
-  onUnload() {
-    // 页面被关闭
-  },
-  onTitleClick() {
-    // 标题被点击
-  },
-  onPullDownRefresh() {
-    // 页面被下拉
-  },
-  onReachBottom() {
-    // 页面被拉到底部
-  },
-  onShareAppMessage() {
-    // 返回自定义分享信息
-    return {
-      title: 'My App',
-      desc: 'My App description',
-      path: 'pages/index/index',
-    };
+  data: {
+    focus: false,
+    inputValue: '',
   },
 
-  onHandleNicknameInput(e) {
+  bindButtonTap() {
+    // blur 事件和这个冲突
+    setTimeout(() => {
+      this.onFocus();
+    }, 100);
+  },
+  onFocus() {
+    this.setData({
+      focus: true,
+    });
+  },
+  onBlur() {
+    this.setData({
+      focus: false,
+    });
+  },
+
+  bindKeyInput(e) {
+    this.setData({
+      inputValue: e.detail.value,
+    });
+  },
+
+  bindHideKeyboard(e) {
+    if (e.detail.value === '123') {
+      // 收起键盘
+      my.hideKeyboard();
+    }
+  },
+
+  handleSearch(e) {
+    console.log('search', e.detail.value);
+    this.setData({
+      search: e.detail.value,
+    });
+  },
+  doneSearch() {
+    console.log('doneSearch', this.data.search);
+    my.hideKeyboard();
+  },
+  clearSearch() {
+    console.log('clear search', this.data.search);
+    this.setData({
+      search: '',
+    });
+  },
+  handleNickNameInput(e) {
     console.log('nickname input', e)
   },
-
-  onHandleNicknameBlur(e) {
+  handleNickNameBlur(e) {
     console.log('nickname blur', e)
   }
 });
