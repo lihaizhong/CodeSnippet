be08f1767fc54ca33c453cc5f48e608ce2e4d86c
lihzsky <lihaizh_cn@foxmail.com>
2024-09-11 13:59:47 +0800

    feat: base64迁移

--- packages/protobuf/src/utils/base64.ts
+++ packages/protobuf/src/utils/base64.ts
@@ -32,7 +32,59 @@ class Base64 {
    * @param {number} end Source end
    * @returns {string} Base64 encoded string
    */
-  encode(buffer: Uint8Array, start: number, end: number) {}
+  encode(buffer: Uint8Array, start: number, end: number): string {
+    const chunk = []
+    let parts = []
+    let i = 0
+    let j = 0
+    let t = 0
+
+    while (start < end) {
+      const b = buffer[start++]
+
+      switch (j) {
+        case 0:
+          chunk[i++] = Base64.ENCODING_TABLE[b >> 2]
+          t = (b & 3) << 4
+          j = 1
+          break
+        case 1:
+          chunk[i++] = Base64.ENCODING_TABLE[t | b >> 4]
+          t = (b & 15) << 2
+          j = 2
+          break
+        case 2:
+          chunk[i++] = Base64.ENCODING_TABLE[t | b >> 6]
+          chunk[i++] = Base64.ENCODING_TABLE[b & 63]
+          j = 0
+          break
+      }
+
+      if (i > 8191) {
+        parts.push(String.fromCharCode(...chunk))
+        i = 0
+      }
+    }
+
+    if (j) {
+      chunk[i++] = Base64.ENCODING_TABLE[t]
+      chunk[i++] = 61
+
+      if (j === 1) {
+        chunk[i++] = 61
+      }
+    }
+
+    if (parts.length) {
+      if (i) {
+        parts.push(String.fromCharCode(...chunk.slice(0, i)))
+      }
+
+      return parts.join('')
+    }
+
+    return String.fromCharCode(...chunk.slice(0, i))
+  }
 
   /**
    * Decodes a base64 encoded string to a buffer.
@@ -42,7 +94,47 @@ class Base64 {
    * @returns {number} Number of bytes written
    * @throws {Error} If encoding is invalid
    */
-  decode(str: string, buffer: Uint8Array, offset: number) {}
+  decode(str: string, buffer: Uint8Array, offset: number): number {
+    let start = offset
+    let j = 0
+    let i = 0
+    let t: number = 0
+
+    while (i < str.length) {
+      let c = str.charCodeAt(i++)
+
+      if (c === 61 && j > 1) {
+        break
+      }
+
+      c = Base64.DECODING_TABLE[c]
+      if (c === undefined) {
+        throw new Error(Base64.INVALID_ENCODING)
+      }
+
+      switch (j) {
+        case 0:
+          t = c
+          j = 1
+          break
+        case 1:
+          buffer[offset++] = t << 2 | (c & 48) >> 4
+          t = c
+          j = 2
+          break
+        case 3:
+          buffer[offset++] = (t & 3) << 6 | c
+          j = 0
+        break
+      }
+    }
+
+    if (j === 1) {
+      throw new Error(Base64.INVALID_ENCODING)
+    }
+
+    return offset - start
+  }
 
   /**
    * Tests if the specified string appears to be base64 encoded.
