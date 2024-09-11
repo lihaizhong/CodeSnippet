a2862255bbf2e79e3b3ff3eac9481f2bab122dd8
sky <854323752@qq.com>
2024-09-12 03:46:15 +0800

    fix: 优化

---
+++ packages/protobuf/src/utils/longbits.ts
@@ -0,0 +1,91 @@
+export class LongBits {
+  /**
+   * Zero hash
+   * @memberof util.LongBits
+   * @type {string}
+   */
+  static zeroHash: string = '\0\0\0\0\0\0\0\0'
+  /**
+   * Constructs new long bits from the specified number.
+   * @param {number} value Value
+   * @returns {util.LongBits} Instance
+   */
+  static fromNumber(value: number) {
+    if (value === 0) {
+      return zero
+    }
+
+    const sign = value < 0
+
+    if (sign) {
+      value = -value
+    }
+
+    let lo = value >>> 0
+    let hi = (value - lo) / 4294967296 >>> 0
+
+    if (sign) {
+      hi = ~hi >>> 0
+      lo = ~lo >>> 0
+
+      if (++lo > 4294967295) {
+        lo = 0
+
+        if (++hi > 4294967295) {
+          hi = 0
+        }
+      }
+    }
+
+    return new LongBits(lo, hi)
+  }
+  static from(value: number | string) {
+    if (typeof value === 'number') {
+      return LongBits.fromNumber(value)
+    }
+
+    if (typeof value === 'string') {
+
+    }
+  }
+  /**
+   * Low bits
+   * @type {number}
+   */
+  readonly lo: number
+  /**
+   * High bits
+   * @type {number}
+   */
+  readonly hi: number
+
+  constructor(lo: number, hi: number) {
+    this.lo = lo >>> 0
+    this.hi = hi >>> 0
+  }
+}
+
+class ZeroBits extends LongBits {
+  toNumber(): number {
+    return 0
+  }
+
+  zzEncode(): ZeroBits {
+    return this
+  }
+
+  zzDecode(): ZeroBits {
+    return this
+  }
+
+  length(): number {
+    return 1
+  }
+}
+
+/**
+ * Zero bits.
+ * @memberof util.LongBits
+ * @type {util.LongBits}
+ */
+const zero = new ZeroBits(0, 0)
